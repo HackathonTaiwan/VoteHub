687e010ffb07e01bd9a95289439cbad88dddac86
Fred Chien <cfsghost@gmail.com>
2015-12-25 10:29:17 +0000

    add API to vote

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ var koa = require('koa');
 var session = require('koa-session');
 var Router = require('koa-router');
 var serve = require('koa-static');
+var bodyParser = require('koa-bodyparser');
+var UAParser = require('ua-parser-js');
+var mysql = require('mysql');
+
+var dbConn = mysql.createConnection(require('./db.json'));
+dbConn.connect();
 
 var app = koa();
 
 // Static file path
 app.use(serve(path.join(__dirname, 'public'), { hidden: true }));
+
+app.use(bodyParser());
 
 // Initializing session and setting it expires in one month
 app.keys = 'ASDasd123v4rfv5tgbyhiujm21ed' || [];
@@ -26,8 +34,42 @@ app.use(function *(next) {
 
 // Routes
 var router = new Router();
-router.get('/', function *() {
+router.get('/xxx', function *() {
+var u = this.request.header['user-agent'];
+console.log(this.request);
 	this.body = 'Hi'
+});
+
+router.post('/vote', function *() {
+	var parser = new UAParser(this.request.header['user-agent']);
+
+	var ip = this.request.header['x-forwarded-for'];
+	var os = parser.getOS().name + parser.getOS().version;
+	var internal_ip = this.request.body.internal_ip || '';
+	var browser = parser.getBrowser().name + parser.getBrowser().version;
+	var target = this.request.body.target || 0;
+	var age = this.request.body.age || 0;
+
+	try {
+		yield function(done) {
+			dbConn.query('INSERT INTO `votes` SET ?', {
+				os: os,
+				ip: ip,
+				internal_ip: internal_ip,
+				browser: browser,
+				target: target,
+				age: age,
+				user_agent: this.request.header['user-agent'] || ''
+			}, done);
+		};
+	} catch(e) {}
+
+	// wait 3 secs for stopping robot
+	yield function(done) {
+		setTimeout(done, 3000);
+	;}
+
+	this.body = {};
 });
 app.use(router.middleware());
 
--- data/votehub.sql
+++ data/votehub.sql
@@ -1,9 +1,13 @@
+CREATE DATABASE votehub;
 CREATE TABLE votes {
 	id int NOT NULL AUTO_INCREMENT,
 	target int NOT NULL,
+	internal_ip VARCHAR(15) NOT NULL,
 	ip VARCHAR(15) NOT NULL,
 	os VARCHAR(32) NOT NULL,
+	browser VARCHAR(32) NOT NULL,
 	age int NOT NULL,
+	user_agent VARCHAR(128) NOT NULL,
 	date datetime NOT NULL DEFAULT NOW(),
 	PRIMARY KEY (id)
 };
